37c33dea08697838a6c10a14bb8e465abb766ea5
Cameron Guthrie <undefined>
2021-04-11 21:07:40 -0600

    fix error in placing mines that allows duplicate mine locations, which leads to incorrect count of nearby mines

--- minesweeper.js
+++ minesweeper.js
@@ -1,22 +1,40 @@
 export const createBoard = (boardSize, numberOfMines) => {
-    let gameBoard = createEmptyBoard(boardSize)
-    const mineLocations = getMineLocations(boardSize, numberOfMines)
+    let gameBoard = createEmptyBoard(boardSize);
+    const mineLocations = getMineLocations(boardSize, numberOfMines);
+    console.log(mineLocations)
     placeMines(gameBoard, mineLocations);
     markMinesNearby(gameBoard, mineLocations);
     return gameBoard;
 }
 
+export const TILE_STATUSES = {
+    HIDDEN: 'hidden',
+    MINE: 'mine',
+    NUMBER: 'number',
+    MARKED: 'marked'
+}
+
 const createEmptyBoard = (boardSize) => {
     const board = new Array(boardSize).fill(0)
     .map((e,y) => new Array(boardSize).fill({y,})
-    .map((tile,i,a) => 
-    tile = {
-        x: i, 
-        y: a[i].y, 
-        mine: false, 
-        hidden: true, 
-        nearbyMines: 0, 
-        marked: false
+    .map((tile,i,a) => {
+        const element = document.createElement("div");
+        element.dataset.status = TILE_STATUSES.HIDDEN;
+        return (
+            tile = {
+                x: i, 
+                y: a[i].y, 
+                mine: false, 
+                nearbyMines: 0, 
+                element,
+                get status() {
+                    return this.element.dataset.status;
+                },
+                set status(value) {
+                    return this.element.dataset.status = value;
+                },
+            }
+        )
     }));
     return board;
 }
@@ -27,22 +45,34 @@ const getMineLocations = (boardSize, numberOfMines) => {
     while (mineLocations.length < numberOfMines) {
         const x = getRandNum(boardSize);
         const y = getRandNum(boardSize);
-        !mineLocations.includes([x,y]) ? mineLocations.push([x,y]) : '';
+        console.log(mineLocations);
+        console.log([x,y],mineLocations.includes([x,y]))
+        console.log(mineLocations.some(e => e.id === `${x}${y}`))
+        if (mineLocations.some(e => e.id === `${x}${y}`)) {
+            console.log('included',[x,y]);
+        } else {
+            mineLocations.push({x,y,id:`${x}${y}`})
+        }
     }
     return mineLocations;
 }
 
 const placeMines = (gameBoard, mineLocations) => {
-    mineLocations.forEach(([mineX,mineY]) => gameBoard[mineY][mineX].mine = true);
+    mineLocations.forEach(location => {
+        const x = location.x;
+        const y = location.y;
+        const tile = gameBoard[y][x];
+        tile.mine = true;
+    })
 }
 
 const markMinesNearby = (gameBoard, mineLocations) => {
-    const isValidNeighbor = ([x,y]) => {
+    const isValidNeighbor = (x,y) => {
+        const boardSize = gameBoard.length;
         const validX = x >= 0 && x < boardSize;
         const validY = y >= 0 && y < boardSize;
-        return validX && validY ? gameBoard[y][x].mine === false : false;
+        return validX && validY ? gameBoard[y][x].mine !== true : false;
     }
-    const boardSize = gameBoard.length;
     const transpositions = {
         n: {x:0,y:-1},
         ne: {x:1,y:-1},
@@ -54,12 +84,19 @@ const markMinesNearby = (gameBoard, mineLocations) => {
         nw: {x:-1,y:-1}
     }
     const directions = ['n','ne','e','se','s','sw','w','nw'];
-    mineLocations.forEach(([mineX,mineY]) => {
+    mineLocations.forEach((location) => {
         directions.forEach(direction => {
-            let [neighborX, neighborY] = [mineX + transpositions[direction].y, mineY + transpositions[direction].x];
-            isValidNeighbor([neighborX,neighborY]) ? gameBoard[neighborY][neighborX].nearbyMines++ : '';
+            const mineX = location.x;
+            const mineY = location.y;
+            const neighborX = mineX + transpositions[direction].x;
+            const neighborY = mineY + transpositions[direction].y;
+            if (isValidNeighbor(neighborX,neighborY)) {
+                const tile = gameBoard[neighborY][neighborX];
+                tile.nearbyMines++;
+                tile.element.textContent = tile.nearbyMines;
+            }
         })
     })
 }
 
-console.log(createBoard(10,10))
+// console.log(createBoard(10,10))
--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-import { createBoard } from './minesweeper.js'
+import { createBoard, TILE_STATUSES } from './minesweeper.js'
 
-console.log(createBoard(10,10))
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 50;
+
+const board = createBoard(BOARD_SIZE,NUMBER_OF_MINES);
+const boardElement = document.querySelector(".board");
+const minesRemainingText = document.querySelector("[data-mine-count]")
+
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element);
+        tile.element.addEventListener("click", () => {
+            revealTile(tile);
+        });
+        tile.element.addEventListener("contextmenu", e => {
+            e.preventDefault();
+            markTile(tile);
+            numMinesLeft(board, NUMBER_OF_MINES);
+        })
+    })
+})
+
+boardElement.style.setProperty("--size", BOARD_SIZE)
+minesRemainingText.textContent = NUMBER_OF_MINES;
+
+const markTile = tile => {
+    const markable = (tile.status === TILE_STATUSES.HIDDEN || tile.status === TILE_STATUSES.MARKED);
+    const noMoreMarksRemain = minesRemainingText.textContent <= 0
+    markable ? 
+        tile.status === TILE_STATUSES.MARKED || noMoreMarksRemain ? 
+            tile.status = TILE_STATUSES.HIDDEN :
+            tile.status = TILE_STATUSES.MARKED :
+        '';
+}
+
+const numMinesLeft = (board, NUMBER_OF_MINES) => {
+    const markedTilesCount = board.reduce((count, row) => {
+        return (count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length)}, 0);
+    minesRemainingText.textContent = NUMBER_OF_MINES - markedTilesCount
+}
+
+const revealTile = (tile) => {
+    const revealable = tile.status === TILE_STATUSES.HIDDEN;
+    const isMine = tile.mine === true;
+    revealable ? isMine ? tile.status = TILE_STATUSES.MINE : tile.status = TILE_STATUSES.NUMBER: '';
+}
+
